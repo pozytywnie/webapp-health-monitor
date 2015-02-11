ce2a1cf5b3970b565a9611acf17836635169f2dd
Marek Siarkowicz <marek.siarkowicz@pozytywnie.pl>
2015-02-11 15:06:36 +0100

    Print errors stack trace.

--- webapp_health_monitor/verification_suit.py
+++ webapp_health_monitor/verification_suit.py
@@ -1,4 +1,6 @@
 import collections
+import traceback
+import sys
 
 from webapp_health_monitor.errors import VerificationFailure
 
@@ -8,44 +10,69 @@ class VerificationSuit(object):
         self.verificators = verificators
 
     def run(self):
-        failures = []
-        errors = []
+        results = []
         for verificator in self.verificators:
             try:
                 verificator.run()
             except VerificationFailure as e:
-                failures.append(VerificatorResultFailure(verificator, e))
-            except Exception as e:
-                errors.append(VerificatorResultError(verificator, e))
-        return VerificationSuitResult(failures=failures, errors=errors)
+                results.append(VerificatorResultFailure(verificator, e))
+            except Exception:
+                type, value, traceback = sys.exc_info()
+                results.append(VerificatorResultError(
+                    verificator, type, value, traceback))
+            else:
+                results.append(VerificatorResultSuccess(verificator))
+        return VerificationSuitResult(results)
 
 
 class VerificationSuitResult(object):
-    def __init__(self, failures, errors):
-        self.failures = failures
-        self.errors = errors
+    def __init__(self, results):
+        self.results = results
 
     def report(self):
-        if self.has_failed():
-            message = 'Failure\n'
-            if self.errors:
-                message += 'Errors\n{}\n'.format(
-                    '\n'.join(map(str, self.errors)))
-            if self.failures:
-                message += 'Failures\n{}\n'.format(
-                    '\n'.join(map(str, self.failures)))
-            return message
-        else:
-            return 'Success\n'
+        report_lines = []
+        for result in self.results:
+            report_lines.append(str(result))
+        errors = list(self.errors)
+        if errors:
+            report_lines.append('')
+            report_lines.append('Errors')
+            for result in errors:
+                report_lines.append(str(result.verificator))
+                stack_trace = traceback.format_exception(
+                    result.type, result.value, result.traceback)
+                report_lines.append(''.join(stack_trace))
+        return '\n'.join(report_lines)
 
     def has_failed(self):
-        return bool(self.errors or self.failures)
+        return bool(list(self.errors)) or bool(list(self.failures))
+
+    @property
+    def errors(self):
+        for result in self.results:
+            if isinstance(result, VerificatorResultError):
+                yield result
+
+    @property
+    def failures(self):
+        for result in self.results:
+            if isinstance(result, VerificatorResultFailure):
+                yield result
+
+
+class VerificatorResultSuccess(object):
+    def __init__(self, verificator):
+        self.verificator = verificator
+
+    def __str__(self):
+        return '{}: OK'.format(self.verificator)
 
 
 class VerificatorResultError(collections.namedtuple(
-        'VerificatorResultErrorBase', ('verificator', 'error'))):
+        'VerificatorResultErrorBase', (
+            'verificator', 'type', 'value', 'traceback'))):
     def __str__(self):
-        return '{}: {}'.format(self.verificator, repr(self.error))
+        return '{}: ERROR'.format(self.verificator)
 
 
 class VerificatorResultFailure(collections.namedtuple(
@@ -53,8 +80,9 @@ class VerificatorResultFailure(collections.namedtuple(
     def __str__(self):
         value_description = self.verificator.value_description
         if value_description:
-            return '{}: {} {}'.format(
+            return '{}: FAILURE {} {}'.format(
                 self.verificator, value_description, self.failure)
         else:
-            return '{}: Value {}'.format(self.verificator, self.failure)
+            return '{}: FAILURE Value {}'.format(
+                self.verificator, self.failure)
 
