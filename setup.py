0d886ff1b09785b36bef1d115357d8625a48aa98
Marek Siarkowicz <marek.siarkowicz@pozytywnie.pl>
2015-08-18 14:03:30 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='webapp-health-monitor',
-    version='0.1.5',
+    version='0.1.6',
     author='Tomasz Wysocki',
     author_email='tomasz@pozytywnie.pl',
     url='https://github.com/pozytywnie/webapp-health-monitor',
